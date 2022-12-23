e830614218a226d2bdbc0b9ddff80bdc8e645414
David Falson <david@falson.net>
2022-12-23 13:29:34 -0800

    created cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, redirect, render_template, flash
+from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+app.config['SECRET_KEY'] = "doggies"
+debug = DebugToolbarExtension(app)
+
+with app.app_context():
+    connect_db(app)
+
+    db.create_all()
